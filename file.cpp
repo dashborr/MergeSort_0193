41c2d462b05bf6900724055311e727f788c74d78
dashborr <danisarfas7@gmail.com>
2026-04-03 14:45:27 +0700

    Menambahkan rekursi merge sort

--- file.cpp
+++ file.cpp
@@ -41,5 +41,9 @@ void mergeSort(int low, int high)
 
     int mid = (low + high) / 2; // step 2
 
+      // step 3 (rekursi)
+    mergeSort(low, mid);       // step 3.a
+    mergeSort(mid + 1, high);  // step 3.b
+
 
 }
